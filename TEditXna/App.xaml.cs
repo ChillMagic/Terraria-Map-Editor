41479bd9ee9afa252dd1f19cfa7e7adee6e65a56
Ziteng Wang <mymisting@gmail.com>
2018-05-21 13:05:38 -0700

    Update cn version

--- TEditXna/App.xaml.cs
+++ TEditXna/App.xaml.cs
@@ -20,7 +20,7 @@ namespace TEditXna
 
         public static FileVersionInfo Version { get; set; }
 		
-		public static Version CnVersion { get; } = new Version(1, 7, 1, 0);
+		public static Version CnVersion { get; } = new Version(1, 8, 0, 0);
 
         protected override void OnStartup(StartupEventArgs e)
         {
